6ef20755581bf9a0b48cc94893ef8c81498d46e5
atligudmunds <a13@ru.is>
2015-10-16 18:34:48 +0000

    Passed test for inputing two numbers seperated by a comma

--- Calculator.java
+++ Calculator.java
@@ -7,8 +7,14 @@ public class Calculator
 		if(text.equals("")) {
 			return 0;
 		}
+		else if(text.contains(",")) {
+			String[] splitString = text.split(",");
+			return Integer.parseInt(splitString[0])
+					+ Integer.parseInt(splitString[1]);
+		}
 
-		else
+		else {
 			return Integer.parseInt(text);
+		}
 	}
 }
