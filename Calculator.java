ea8fd55353aa5a0c0679742a3218ee475f8051e2
atligudmunds <a13@ru.is>
2015-10-17 18:36:34 +0000

    Cleaned code a little, but only a little

--- Calculator.java
+++ Calculator.java
@@ -13,12 +13,9 @@ public class Calculator
 			if(text.charAt(2) == '[')
 			{
 				int endDelimIndex = text.indexOf(']', 3);
-				//System.out.println("Index of block:" + endDelimIndex);
 				String delimiter = text.substring(3, endDelimIndex);
-				//System.out.println("Delimiter:" + delimiter);
 
 				String numberString = text.substring(endDelimIndex + 2);
-				//System.out.println("Number String:" + numberString);
 				String[] splitString = numberString.split(delimiter);
 				try {
 					return theSum(splitString);
@@ -43,9 +40,7 @@ public class Calculator
 
 		else if(text.contains(",") || text.contains("\n")) {
 			String[] splitString = text.split(",|\\\n");
-			//return theSum(splitString);
 			try {
-				//theSum summa = new theSum();
 				return theSum(splitString);
 			}
 			catch (Exception e){
@@ -60,7 +55,7 @@ public class Calculator
 		int numOfNumber = splitString.length;
 		int totalSum = 0;
 		StringBuilder negativeString = new StringBuilder();
-		//System.out.println("LOOP START");
+
 		for(int i = 0; i < numOfNumber; i++)
 		{
 			int parsedInt = Integer.parseInt(splitString[i]);
@@ -70,15 +65,12 @@ public class Calculator
 			if(parsedInt < 0) {
 				negativeString.append(Integer.toString(parsedInt) + ", ");
 			}
-			//System.out.println(parsedInt);
 			totalSum += parsedInt;
 		}
-		//System.out.println("LOOP END");
 		if(negativeString.length() > 0) {
 			negativeString.setLength(negativeString.length() -2);
 			throw new Exception("Negatives not allowed: " + negativeString);			
 		}
-		//System.out.println("TOTAL SUM:" + totalSum);
 		return totalSum;
 	}
 }
